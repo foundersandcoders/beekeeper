a97bb8c9835eb9b9eb537c2a75e89e33b71b0c40
besartshyti <besartshyti@MacBook-Air-di-Besart.local>
2015-04-23 12:19:34 +0100

    test message

---
+++ models/member.js
@@ -0,0 +1,30 @@
+"use strict";
+
+
+// var Joi = require("joi");
+
+// module.exports = {
+// 	payload: {
+// 		memberId:      Joi.string().required(),
+// 		title:      Joi.date().iso(),
+// 		initials:  Joi.number(),
+// 		firstName:      Joi.number(),
+// 		lastName:        Joi.string(),
+// 		address:         Joi.forbidden(), // don't allow to set this!
+// 		postCodes:         Joi.string(),
+// 		deliverer:          Joi.string().valid(validateTypes),
+// 		phones: Joi.string() // lets make it an object
+// 		DoB:         Joi.string().optional(),
+// 		age:
+// 		emails: // array,
+// 		emailBounced
+// 		dateJoined:
+// 		membershipType: "lifeSingle",
+// 		dateMembershipTypeChanged: "2015-04-22",
+// 		gad: true,
+// 		dateGadSigned: "2015-01-3",
+// 		paysByStandingOrder: true,
+// 		onlineMember: false,
+// 		membershipNotes: "This guy is pretty cool"
+// 	}
+// };
